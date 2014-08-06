16ede3f143db4de8bcb3e304458998f4083193d3
Ariel Nunez <ingenieroariel@gmail.com>
2014-08-06 14:20:21 -0500

    Refactored code
    - The file is flatter now. Because flat is better than nested.
    - The processing ends when the file has no more frames.
    - try/except blocks were removed with more readable code.

--- count.py
+++ count.py
@@ -24,27 +24,42 @@ capture = cv2.VideoCapture(options.filename)
 best_id=0
 i = 0
 
-if capture:
-  while True:
+keep_processing = True
 
+while(keep_processing):
     ret, frame = capture.read()
-    if ret:
-        fgmask = backsub.apply(frame, None, 0.01)
-        contours, hierarchy = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL,
-                                           cv2.CHAIN_APPROX_NONE)
-        try: hierarchy = hierarchy[0]
-        except: hierarchy = []
-        for contour, hier in zip(contours, hierarchy):
-            (x,y,w,h) = cv2.boundingRect(contour)
-            if w > 20 and h > 20:
-                # figure out id
-                best_id+=1
-                cv2.rectangle(frame, (x,y), (x+w,y+h), (255, 0, 0), 2)
-                cv2.putText(frame, str(best_id), (x,y-5), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5, (255, 0, 0), 2)
-        print(best_id)        
-        cv2.imshow("Track", frame)
-        cv2.imshow("background sub", fgmask)
+
+    # When the stream has no more frames, stop processing.
+    if frame is None:
+        keep_processing = False
+        break
+
+    fgmask = backsub.apply(frame, None, 0.01)
+    contours, hierarchy_vector = cv2.findContours(fgmask.copy(),
+                                                  cv2.RETR_EXTERNAL,
+                                                 cv2.CHAIN_APPROX_NONE)
+
+    hierarchy = []
+
+    if hierarchy_vector is not None:
+        hierarchy = hierarchy_vector[0]
+ 
+    # Iterate over the contours to draw the rectangles.
+    for contour, hier in zip(contours, hierarchy):
+        (x,y,w,h) = cv2.boundingRect(contour)
+        if w > 20 and h > 20:
+            # figure out id
+            best_id+=1
+            cv2.rectangle(frame, (x,y), (x+w,y+h), (255, 0, 0), 2)
+            cv2.putText(frame, str(best_id), (x,y-5), cv2.FONT_HERSHEY_SIMPLEX,
+               0.5, (255, 0, 0), 2)
+
+    print(best_id)
+    cv2.imshow("Track", frame)
+    cv2.imshow("background sub", fgmask)
+
+    # Way for user input, if the user presses q then stop processing.
     key = cv2.waitKey(10)
     if key == ord('q'):
-            break
+        keep_processing = False
+        break
