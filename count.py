b7786fcd19e0a6a770a78d2504540b84c156bfe3
Ariel Nunez <ingenieroariel@gmail.com>
2014-08-06 14:39:43 -0500

    Ya hace algo

--- count.py
+++ count.py
@@ -21,10 +21,11 @@ if options.filename is None:
 
 backsub = cv2.BackgroundSubtractorMOG()
 capture = cv2.VideoCapture(options.filename)
-best_id=0
-i = 0
 
 keep_processing = True
+# Set horizon line (as a fraction of the height).
+# 0.3 means 30% from the top
+HORIZON_LINE_RATIO = 0.6
 
 while(keep_processing):
     ret, frame = capture.read()
@@ -34,6 +35,8 @@ while(keep_processing):
         keep_processing = False
         break
 
+    height, width, __ = frame.shape
+    horizon = int(height * HORIZON_LINE_RATIO)
     fgmask = backsub.apply(frame, None, 0.01)
     contours, hierarchy_vector = cv2.findContours(fgmask.copy(),
                                                   cv2.RETR_EXTERNAL,
@@ -44,22 +47,23 @@ while(keep_processing):
     if hierarchy_vector is not None:
         hierarchy = hierarchy_vector[0]
  
+    # Draw counting line.
+    cv2.line(frame, (0, horizon), (width, horizon), (255, 255, 255), 1)
+
     # Iterate over the contours to draw the rectangles.
     for contour, hier in zip(contours, hierarchy):
         (x,y,w,h) = cv2.boundingRect(contour)
-        if w > 20 and h > 20:
-            # figure out id
-            best_id+=1
+
+        if y < horizon < y + h:
             cv2.rectangle(frame, (x,y), (x+w,y+h), (255, 0, 0), 2)
-            cv2.putText(frame, str(best_id), (x,y-5), cv2.FONT_HERSHEY_SIMPLEX,
+            cv2.putText(frame, "x=%s" % x, (x,y-5), cv2.FONT_HERSHEY_SIMPLEX,
                0.5, (255, 0, 0), 2)
 
-    print(best_id)
     cv2.imshow("Track", frame)
     cv2.imshow("background sub", fgmask)
 
     # Way for user input, if the user presses q then stop processing.
-    key = cv2.waitKey(10)
+    key = cv2.waitKey(100)
     if key == ord('q'):
         keep_processing = False
         break
